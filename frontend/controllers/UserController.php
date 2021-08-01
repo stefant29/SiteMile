cbadbdfca300ed1918f72f07cb048d1ab23cde8c
Stefan Toncu <stefan.toncu29@gmail.com>
2021-08-01 14:35:23 +0300

    Add User CRUD

---
+++ frontend/controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use common\models\UserSearch;
+use yii\web\Controller;
+
+/**
+ * UserController implements the CRUD actions for User model.
+ */
+class UserController extends Controller
+{
+
+    /**
+     * Lists all User models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new UserSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+}
---
+++ frontend/views/user/index.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\UserSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Users';
+$this->params['breadcrumbs'][] = $this->title;
+
+?>
+<div class="user-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?php Pjax::begin(); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            'username',
+            'email:email',
+        ],
+    ]); ?>
+
+    <?php Pjax::end(); ?>
+
+</div>
